40952132d61862f3357fa05b11af9b1540d03217
hengstchon <hengstchon@gmail.com>
2022-04-16 10:17:36 +0200

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Import Lua modules
+require('packer_init')
+
+require('core/settings')
+require('core/keymaps')
+require('core/statusline')
+
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-treesitter')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-cmp')
---
+++ lua/packer_init.lua
@@ -0,0 +1,101 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Plugin manager: packer.nvim
+-- url: https://github.com/wbthomason/packer.nvim
+
+-- For information about installed plugins see the README:
+-- neovim-lua/README.md
+-- https://github.com/brainfucksec/neovim-lua#readme
+
+
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- Install plugins
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim' -- packer can manage itself
+
+  -- Color schemes
+  use 'navarasu/onedark.nvim'
+
+  -- File explorer
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Icons
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- Indent line
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Autopair
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  }
+
+  -- Treesitter interface
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+
+  -- Autocomplete
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  }
+
+  -- Statusline
+  use {
+    'famiu/feline.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  -- git labels
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  -- fuzzy finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
