19cf15b22f87fc0f3bcf5461d4c15c9f234889c1
hengstchon <hengstchon@gmail.com>
2022-06-04 21:08:36 +0200

    fix: update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,21 +13,6 @@
 -- Default options ARE NOT included.
 -- See: `:help NvimTree`
 
-
--- Global options
-local g = vim.g
-
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_git_hl                 = 1
-g.nvim_tree_respect_buf_cwd        = 1
-g.nvim_tree_width_allow_resize     = 1
-g.nvim_tree_icons                  = { default = "" }
-g.nvim_tree_show_icons             = {
-  git = 1,
-  folders = 1,
-  files = 1,
-}
-
 local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
@@ -36,25 +21,17 @@ end
 -- Call setup:
 --- Each of these are documented in `:help nvim-tree.OPTION_NAME`
 nvim_tree.setup {
-  open_on_setup = true,
   update_cwd = true,
-  view = { width = 32 },
+  respect_buf_cwd = true,
   renderer = {
-    indent_markers = {
-      enable = false,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
-  },
-  actions = {
-    change_dir = { enable = false },
+    highlight_opened_files = 'name',
   },
   update_focused_file = {
     enable = true,
     update_cwd = true,
+  },
+  actions = {
+    change_dir = { enable = false },
   },
   filters = {
     dotfiles = true,
