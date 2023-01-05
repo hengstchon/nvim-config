ae919d823191912181f88f715b686fe7cffeedf3
hengstchon <hengstchon@gmail.com>
2023-01-05 01:01:11 +0100

    update gitsigns

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -28,17 +28,16 @@ gitsigns.setup {
     end, { expr = true })
 
     -- Actions
-    map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'GS: stage hunk' })
+    map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'GS: reset hunk' })
+    map('n', '<leader>hS', gs.stage_buffer, { desc = 'GS: stage buffer' })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'GS: undo stage hunk' })
+    map('n', '<leader>hR', gs.reset_buffer, { desc = 'GS: reset buffer' })
+    map('n', '<leader>hp', gs.preview_hunk, { desc = 'GS: preview hunk' })
+    map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'GS: blame line' })
+    map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'GS: toggle current line blame' })
+    map('n', '<leader>hd', gs.diffthis, { desc = 'GS: diff this' })
+    map('n', '<leader>td', gs.toggle_deleted, { desc = 'GS: toggle deleted' })
 
     -- Text object
     map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
