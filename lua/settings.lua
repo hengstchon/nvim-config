b05a2442af9c1ed7d7019b2bc672e9b4093276d9
hengstchon <hengstchon@gmail.com>
2023-01-06 14:56:07 +0100

    update autocmd

--- lua/settings.lua
+++ lua/settings.lua
@@ -25,11 +25,8 @@ vim.opt.shortmess:append 'I' -- Disable nvim intro
 
 
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  pattern = '*',
   callback = function() vim.highlight.on_yank() end,
-  group = highlight_group,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
