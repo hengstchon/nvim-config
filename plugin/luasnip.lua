1602a9b81df786acf27730c0fa77616aa47b3099
hengstchon <hengstchon@gmail.com>
2023-01-06 13:21:25 +0100

    update luasnip

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -5,8 +5,12 @@ if not ok then
 end
 
 luasnip.config.set_config({
+  updateevents = 'TextChanged,TextChangedI',
   region_check_events = 'InsertEnter',
   delete_check_events = 'InsertLeave'
 })
 
 require('luasnip.loaders.from_vscode').lazy_load()
+-- require('luasnip.loaders.from_vscode').lazy_load({
+--   paths = { './snippets' },
+-- })
