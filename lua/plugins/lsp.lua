eb02e01741a3d1fce700abe184cd5c1a5b094c4c
hengstchon <hengstchon@gmail.com>
2023-06-02 12:47:01 +0200

    fix: nerd font icons & update packages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,7 +23,8 @@ return {
     },
     config = function()
       --- diagnostics settings
-      local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+      local signs = { Error = "󰅚", Warn = "󰀪", Hint = "󰌶", Info = "" }
+
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
