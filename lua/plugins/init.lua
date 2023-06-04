bcb6527e4ab9d7e2b1d895985cc1d89855d5fbd4
hengstchon <hengstchon@gmail.com>
2023-06-04 14:48:55 +0200

    add outline plugin: aerial.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
-  'nvim-lua/plenary.nvim',        -- Common utilities
-  'kyazdani42/nvim-web-devicons', -- Icons
+  'nvim-lua/plenary.nvim',       -- Common utilities
+  'nvim-tree/nvim-web-devicons', -- Icons
 
   -- Colorscheme
   {
@@ -76,6 +76,24 @@ return {
     'kylechui/nvim-surround',
     event = 'VeryLazy',
     config = true,
+  },
+
+  -- outline
+  {
+    'stevearc/aerial.nvim',
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = {
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+      end
+    },
+    keys = {
+      { "<leader>at", "<cmd>AerialToggle!<CR>", desc = "Aerial Toggle" },
+    },
   },
 
   -- git labels
