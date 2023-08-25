7fe761ef8996bad37dade232c4c50d17aa15ee23
hengstchon <hengstchon@gmail.com>
2023-08-25 11:37:04 +0200

    add nvim-treesitter/playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
       'nvim-treesitter/nvim-treesitter-textobjects',
       'JoosepAlviste/nvim-ts-context-commentstring',
       'windwp/nvim-ts-autotag',
+      'nvim-treesitter/playground',
     },
     config = function()
       require('nvim-treesitter.configs').setup {
@@ -66,6 +67,24 @@ return {
         },
         autotag = {
           enable = true,
+        },
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
         },
       }
     end,
